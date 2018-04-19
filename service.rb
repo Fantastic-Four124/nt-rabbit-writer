1ad42cc8834889b580423159468112e077ce4344
Tom Willkens <tom.willkens@gmail.com>
2018-04-18 20:47:22 -0400

    added worker

--- service.rb
+++ service.rb
@@ -3,8 +3,10 @@ require 'sinatra'
 require_relative 'writer_server'
 
 # DB Setup
-Mongoid.load! "config/mongoid.yml"
+#Mongoid.load! "config/mongoid.yml"
 
 #set binding
 
+set :bind, '0.0.0.0' # Needed to work with Vagrant
+set :port, 8085
 
